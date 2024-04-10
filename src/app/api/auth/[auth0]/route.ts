3118cafa0d7d1edbc180ea34b33d9316ae9ef83d
Mateusz <mateusz.bogacz@cyfrowafoto.com>
2024-04-10 13:20:34 +0200

    fix: runtime edge

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,4 +1,4 @@
 import { handleAuth } from "@auth0/nextjs-auth0";
-
+export const runtime = "edge";
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 export const GET = handleAuth();
